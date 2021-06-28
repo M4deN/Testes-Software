e7aeea79e4e3e5abca677fa6241268b0140b8137
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-06-28 09:18:36 -0300

    Create Candidato.java

---
+++ Exercicio2/src/Pack1/Candidato.java
@@ -0,0 +1,14 @@
+package Pack1;
+
+public class Candidato {
+	
+	 char sexo;
+	 int idade;
+	 public Candidato(char sexo, int idade) {
+	 this.sexo = sexo;
+	 this.idade = idade;
+	 }
+	 public char getSexo() { return sexo; }
+	 public int getIdade() { return idade; }
+	}
+	
