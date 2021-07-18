ce37e567b683840b247e5476444c1f1bab83e045
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-07-18 12:21:08 -0300

    Create VerificadorDeCodigos.java

---
+++ ExercicioMocking1/src/inicial/VerificadorDeCodigos.java
@@ -0,0 +1,9 @@
+package inicial;
+
+public interface VerificadorDeCodigos {
+	
+    public boolean verificarCodigoDisciplina(String codigo);
+    
+    public boolean verificarCodigoTurma(String codigo);
+}
+
