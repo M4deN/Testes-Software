867ea107be56a93d379c64541db9037a08cff044
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-07-18 12:20:52 -0300

    Create TransformTest.java

---
+++ ExercicioMocking1/Mocking10/test/MockingEX10/TransformTest.java
@@ -0,0 +1,14 @@
+package MockingEX10;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+
+class TransformTest {
+
+	@Test
+	void testMeesageOriginal() {
+		
+	}
+
+}
