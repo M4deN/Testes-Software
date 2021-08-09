4a81538de91102a80d6bd67a3229f54a8540f83f
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-08-09 10:28:32 -0300

    Create MessageTransformer.java

---
+++ ExercicioMocking2/src/Mocking/MessageTransformer.java
@@ -0,0 +1,30 @@
+package Mocking;
+
+public class MessageTransformer {
+	
+		private Logger logger;
+	    private Helper helper;
+	    
+	    public MessageTransformer(Logger logger, Helper helper) {
+	   	 
+	   	 this.logger = logger;
+	   	 this.helper = helper;
+	    }
+	    public String transform(String message) {
+	    	
+	   	 String originalMessage = message;
+	   	 
+	   	 for(String censoredWorld : helper.getCensoredWorlds()) {
+	   		 
+	   		 message = message.replaceAll(censoredWorld, "???");
+	   	 }
+	   	 if(!message.equals(originalMessage)){
+	   		 
+	   		 if(!logger.logTransformation(originalMessage, message))
+	   			 
+	   			 throw new RuntimeException("No Conection to database");
+	   	 }
+	   	 return message;
+	    }
+
+}
