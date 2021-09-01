7c6e4ac9eceab70ef8c53687eb34e5ae3a7926fd
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-09-01 07:51:22 -0300

    Create SimuladorDeEventos.java

---
+++ Exercicio1TesteBaseadoEmModelo/src/Modelo/SimuladorDeEventos.java
@@ -0,0 +1,30 @@
+package Modelo;
+
+public class SimuladorDeEventos {
+
+
+	 //os metodos a seguir retornam true se conseguiram fazer a operação e false, caso contrário
+	 public boolean status() {
+		 
+		 return true; 
+	}
+	 public boolean eventA() {
+		 
+		 return true; 
+	}
+	 public boolean eventB() {
+		 
+		 return true; 
+		 
+	}
+	 public boolean eventC() {
+		 
+		 return true; 
+		 
+	}
+	 public boolean eventD() {
+		 
+		 return true; 
+		 
+	}
+}
