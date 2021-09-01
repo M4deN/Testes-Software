4c513e9ae4a762a95c239235a171ff7b88833e40
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-09-01 07:56:53 -0300

    Create MontanhaRussaControlador.java

---
+++ Exercicio4_CaixaPreta_PCE_AVL/src/Exercicio12/MontanhaRussaControlador.java
@@ -0,0 +1,21 @@
+package Exercicio12;
+
+public class MontanhaRussaControlador {
+
+	 ClienteDao clienteDao;
+
+	 public MontanhaRussaControlador(ClienteDao pClienteDao) {
+		 
+		 clienteDao = pClienteDao;
+	 
+	 }
+
+	 public String autorizar(String nome, int idade) throws Exception {
+		 
+		 //todo
+		 
+		 return "";
+	 
+	 }
+	 
+}
