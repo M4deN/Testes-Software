0f6926b485a1c948b1127a2da6f24cf95069838d
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-09-02 08:09:24 -0300

    Create VerificadorDeSenhas.java

---
+++ Exercicio1_CaixaPreta_PCE/src/CaixaPreta/VerificadorDeSenhas.java
@@ -0,0 +1,15 @@
+package CaixaPreta;
+
+public class VerificadorDeSenhas {
+
+	 Dicionario dic;
+	 public VerificadorDeSenhas(Dicionario dic) {
+	 this.dic = dic;
+	 }
+
+	 public boolean validarNovaSenha(String senhaFornecida) {
+	 //TODO
+	 return true;
+	 }
+	}
+
