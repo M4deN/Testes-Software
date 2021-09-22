010a4683bb3a462cd0a39cde28c85f82c27394d5
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-09-22 07:24:43 -0300

    Delete VerificadorDeCodigos.java

--- ExercicioMocking1/src/inicial/VerificadorDeCodigos.java
+++
@@ -1,9 +0,0 @@
-package inicial;
-
-public interface VerificadorDeCodigos {
-	
-    public boolean verificarCodigoDisciplina(String codigo);
-    
-    public boolean verificarCodigoTurma(String codigo);
-}
-
