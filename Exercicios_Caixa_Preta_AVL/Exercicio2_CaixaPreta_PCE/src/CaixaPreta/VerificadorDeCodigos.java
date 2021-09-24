fcaa9f05c929b680c89226438891f40692c0198f
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-09-24 07:24:51 -0300

    Create VerificadorDeCodigos.java

---
+++ Exercicios_Caixa_Preta_AVL/Exercicio2_CaixaPreta_PCE/src/CaixaPreta/VerificadorDeCodigos.java
@@ -0,0 +1,7 @@
+package CaixaPreta;
+
+public interface VerificadorDeCodigos {
+	 public boolean verificarCodigoDisciplina(String codigo);
+	 public boolean verificarCodigoTurma(String codigo);
+	}
+
