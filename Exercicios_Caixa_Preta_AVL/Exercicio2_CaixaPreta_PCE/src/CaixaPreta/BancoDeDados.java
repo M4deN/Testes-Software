38b0796afbef951928268a1bbfb7da7cfac89082
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-09-24 07:24:49 -0300

    Create BancoDeDados.java

---
+++ Exercicios_Caixa_Preta_AVL/Exercicio2_CaixaPreta_PCE/src/CaixaPreta/BancoDeDados.java
@@ -0,0 +1,15 @@
+package CaixaPreta;
+
+public class BancoDeDados {
+	 private VerificadorDeCodigos verificador;
+
+	 public BancoDeDados(VerificadorDeCodigos verificador) {
+	 this.verificador = verificador;
+	 }
+
+	 public String cadastrarTurma(String codDisciplina, String codTurma, int numeroAlunos) {
+	 //todo
+	 return "";
+	 }
+	}
+
