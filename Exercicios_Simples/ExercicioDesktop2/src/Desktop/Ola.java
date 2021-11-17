39bb74d1c13d1badc6b1f24f6f45240cf9288239
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-11-17 09:01:13 -0300

    Create Ola.java

---
+++ Exercicios_Simples/ExercicioDesktop2/src/Desktop/Ola.java
@@ -0,0 +1,25 @@
+package Desktop;
+
+public class Ola {
+
+	String cumprimenta(int a) {
+		
+		if (a == 1) {
+			
+			return "Olá, como você está??";
+		}
+		
+		if(a ==2) {
+			
+			return "Oi tudo bem com você?";
+		}
+		if(a ==3) {
+			
+			return "Hi, how are you?";
+		}
+		else {
+			
+			return "digite um numero entre 1 e 3";
+		}
+	}
+}
