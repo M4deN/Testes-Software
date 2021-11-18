bd6bfee579d71058a6f3118d5150ceb5b4d63dfa
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-11-18 07:59:04 -0300

    Create Inteiro.java

---
+++ Exercicios_Simples/ExercicioDesktop3/src/Desktop/Inteiro.java
@@ -0,0 +1,28 @@
+package Desktop;
+
+public class Inteiro {
+
+	int valor;
+	
+	void carregaValor(int v) {
+		
+		valor = v;
+	}
+	
+	int devolveValor() {
+		
+		return valor;
+	}
+
+	int devolveValorAbsoluto() {
+		
+		return valor;
+	}
+	
+	int imprime() {
+		
+		return valor;
+	}
+		
+
+}
